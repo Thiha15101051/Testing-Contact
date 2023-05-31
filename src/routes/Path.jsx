a6c4a7f45c293afe896534d7eced78f9c0f43c8e
Pyae-Sone-Phone-Myint <codewithme201@gmail.com>
2023-05-31 21:46:59 +0630

    app launch

---
+++ src/routes/Path.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+
+const Path = () => {
+  return (
+    <>
+
+      <Routes>
+        
+      </Routes>
+    </>
+  )
+}
+
+export default Path
