cfb1d8d792f2a60c0b4b8b235697184f4b056e6f
Thiha15101051 <at2330227@gmail.com>
2023-06-05 22:50:04 +0630

    error 12

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -65,13 +65,36 @@ const Navbar = () => {
         {/* After authentication */}
         <div className={`flex items-center gap-5 ${auth ? "block" : "hidden"}`}>
           <h3 className=" hidden lg:flex">{user?.name}</h3>
+          {/* <Menu shadow="lg" width={100} position="left-end">
+            <Menu.Target>
+              <Button className=" hover:text-gray-800">
+                <FaUserCircle size={"2.5rem"} />
+              </Button>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item
+                className=" text-center  font-semibold"
+                onClick={() => nav("/user-detail")}
+              >
+                Profile
+              </Menu.Item>
+              <Menu.Item
+                onClick={() => logOutHandler()}
+                color="red"
+                className=" text-center font-semibold"
+              >
+                Log out
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu> */}
           <Menu>
             <MenuHandler>
               <Avatar
                 variant="circular"
                 alt="candice wu"
+                size="sm"
                 className="cursor-pointer"
-                src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                src="https://static.vecteezy.com/system/resources/previews/005/544/718/original/profile-icon-design-free-vector.jpg"
               />
             </MenuHandler>
             <MenuList>
@@ -82,28 +105,9 @@ const Navbar = () => {
                 </Typography>
               </MenuItem>
               <MenuItem className="flex items-center gap-2">
-                <Cog6ToothIcon strokeWidth={2} className="h-4 w-4" />
+                <UserCircleIcon strokeWidth={2} className="h-4 w-4" />
                 <Typography variant="small" className="font-normal">
-                  Edit Profile
-                </Typography>
-              </MenuItem>
-              <MenuItem className="flex items-center gap-2">
-                <InboxArrowDownIcon strokeWidth={2} className="h-4 w-4" />
-                <Typography variant="small" className="font-normal">
-                  Inbox
-                </Typography>
-              </MenuItem>
-              <MenuItem className="flex items-center gap-2">
-                <LifebuoyIcon strokeWidth={2} className="h-4 w-4" />
-                <Typography variant="small" className="font-normal">
-                  Help
-                </Typography>
-              </MenuItem>
-              <hr className="my-2 border-blue-gray-50" />
-              <MenuItem className="flex items-center gap-2 ">
-                <PowerIcon strokeWidth={2} className="h-4 w-4" />
-                <Typography variant="small" className="font-normal">
-                  Sign Out
+                  My Profile
                 </Typography>
               </MenuItem>
             </MenuList>
